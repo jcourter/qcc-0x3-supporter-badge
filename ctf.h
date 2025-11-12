75c9206c6096d118437e968c0e3c900945016324
jcourter <30299694+jcourter@users.noreply.github.com>
2025-11-12 11:28:38 -0500

    CTF reveal
    Unveiled the CTF message and added a walkthrough for solving it

---
+++ ctf.h
@@ -0,0 +1,2 @@
+#define CTF_SECRET_MESSAGE_PREAMBLE   F("      C C C set your pins to b2 i i set your pins to b2  B  ")
+#define CTF_SECRET_MESSAGE            F("17 19 1 1 13 26 8 20 22 26 14 13 12 14 19 9 6 9 8 11 1 20 14 15 1 10 8 13 6 22 14 19 20 14 2 1 18 19 15 1 20 14 6 15 8 13 11 22 14 19 15 14 21 25 9 20 8 13 1 S")
